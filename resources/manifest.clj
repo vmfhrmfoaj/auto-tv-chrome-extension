f318ccaef8460e0705ad7d57b3b310900ba2dab6
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2016-07-16 02:25:35 +0900

    Cleanup the `resources/manifest.clj`

--- resources/manifest.clj
+++ resources/manifest.clj
@@ -3,45 +3,27 @@
             [clojure.data.json :as json]
             [clojure.string :as str]))
 
-(def template
-  {:name nil
-   :description nil
-   :version nil
-   :permissions ["tabs" "http://*/*" "https://*/*"]
-   :background {:scripts []}
-   :content-scripts [{:matches ["*://*/*"] :js []}]
-   :manifest_version 2})
-
-(let [get-build #(->> %1 (filter (comp (partial = %2) :id)) (first))
-      project (-> "project.clj"
-                  (slurp)
-                  (edn/read-string))
-      name    (str (nth project 1))
-      version (-> project
-                  (nth 2)
-                  (str/split #"-")
-                  (first))
-      project (->> project (drop 3) (apply hash-map))]
-  (-> template
-      (assoc :name name)
-      (assoc :description (get project :description))
-      (assoc :version version)
-      (assoc-in [:background :scripts 0]
-                (-> project
-                    (get-in [:cljsbuild :builds])
-                    (get-build "background")
-                    (get-in [:compiler :output-to])
-                    (str/split #"/")
-                    (rest)
-                    (->> (interpose "/") (apply str))))
-      (assoc-in [:content-scripts 0 :js 0]
-                (-> project
-                    (get-in [:cljsbuild :builds])
-                    (get-build "content")
-                    (get-in [:compiler :output-to])
-                    (str/split #"/")
-                    (rest)
-                    (->> (interpose "/") (apply str))))
-      (json/write-str :escape-slash false
-                      :key-fn (comp #(str/replace % #"-" "_") clojure.core/name))
-      (->> (spit "resources/manifest.json"))))
+(let [proj (-> "project.clj"
+               (slurp)
+               (edn/read-string))
+      [_ name version] proj
+      proj (->> proj (drop 3) (apply hash-map))
+      get-build #(->> %1 (filter (comp (partial = %2) :id)) (first))
+      get-js #(-> proj
+                  (get-in [:cljsbuild :builds])
+                  (get-build %1)
+                  (get-in [:compiler :output-to])
+                  (str/split #"/")
+                  (rest)
+                  (->> (interpose "/") (apply str)))
+      manifest {:name (str name)
+                :description (get proj :description)
+                :version (-> version (str/split #"-") (first))
+                :permissions ["tabs" "http://*/*" "https://*/*"]
+                :background {:scripts [(get-js "background")]}
+                :content-scripts [{:matches ["*://*/*"] :js [(get-js "content")]}]
+                :manifest_version 2}]
+  (spit "resources/manifest.json"
+        (json/write-str manifest
+                        :escape-slash false
+                        :key-fn (comp #(str/replace % #"-" "_") clojure.core/name))))
